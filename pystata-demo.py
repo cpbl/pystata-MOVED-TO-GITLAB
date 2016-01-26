94f243085b6fbd8e8db5659c146f75ab7ca37858
Chris Barrington-Leigh <cpbl@alum.mit.edu>
2016-01-25 19:52:35 -0500

    + small comment

--- pystata-demo.py
+++ pystata-demo.py
@@ -9,7 +9,6 @@ print WP
 print WP+'logs/'
 
 # Run this python file at least two-three times, to generate PDF with results.
-# You need to have cpblDefaults.py in the path (still as of Oct 2014)
 # You need to have cpblUtilities installed in your python path
 # You need to have cpbl-tables installed, both the python files and the LaTeX .sty files, in your python and texmf paths.
 
@@ -22,6 +21,7 @@ latex.skipSavingExistingFigures=False
 import os
 stataout=pst.doHeader+pst.stataLoad(os.getcwd()+'/sample_data_BES')
 dmodels=latex.str2models("""
+* Note: this string is working Stata code. However, it will get heavily preprocessed by pystata.
 *flag:clustering=wards
 reg aq1 aq5a aq6a aq7a [pw=aweightt], cluster(awardid)
 *|
@@ -32,6 +32,8 @@ reg aq1 aq5a aq6a aq7a [pw=aweightt],
 stataout+=latex.regTable('simple demo',dmodels,returnModels=True,transposed='both')
 latex.closeAndCompile()
 pst.stataSystem(stataout,filename=WP+'demos')
+print '\n\n The following is the list of model dicts: \n'
+print dmodels
 
 # If the above results in a PDF with coloured regression tables, then you've the basics working together.
 # There are many more features to be demo'd, which could be added in here
