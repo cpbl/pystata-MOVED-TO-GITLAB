c145aa8ab077f9c0c4e19f9040d971cf7de7a3f5
Chris Barrington-Leigh <cpbl@alum.mit.edu>
2016-01-27 11:11:11 -0500

    + Get the ivreg2 post-estimate tests from the e() estimates table output that was already created. + Give default order and substitution for these parametres (widtest, jp, idp)

--- pystata-demo.py
+++ pystata-demo.py
@@ -24,12 +24,15 @@ dmodels=latex.str2models("""
 * Note: this string is working Stata code. However, it will get heavily preprocessed by pystata.
 *flag:clustering=wards
 reg aq1 aq5a aq6a aq7a [pw=aweightt], cluster(awardid)
-*|
 reg aq1 aq5a aq6a aq7a [pw=aweightt], beta
+*|
 *name:Special
 reg aq1 aq5a aq6a aq7a [pw=aweightt],
 """)
 stataout+=latex.regTable('simple demo',dmodels,returnModels=True,transposed='both')
+
+latex.regTable('simple demo',dmodels,showModels=[mm for mm in dmodels if mm['name'] in ['Special'] or mm.get('flags',{}).get('clustering','') in ['wards']],transposed='both', extraTexFileSuffix='-subset', skipStata=True)
+
 latex.closeAndCompile()
 pst.stataSystem(stataout,filename=WP+'demos')
 print '\n\n The following is the list of model dicts: \n'
